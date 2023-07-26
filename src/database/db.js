67abbd70d298070579b178148da11006e6af2033
douglas.dmc <douglasmarques37@gmail.com>
2023-07-26 13:36:48 -0300

    dotenv

--- src/database/db.js
+++ src/database/db.js
@@ -1,9 +1,16 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const hostname = process.env.DB_HOST
+const dataport = process.env.DB_PORT
+const database = process.env.DB_NAME
 
 const connectDatabase = () => {
     console.log('Wait connecting to the database')
-
-    mongoose.connect('mongodb://127.0.0.1:27017/breakingnews', {useNewUrlParser: true})
+    
+    mongoose.connect(`mongodb://${hostname}:${dataport}/${database}`, {useNewUrlParser: true})
     .then(() => console.log('MongoDB connected'))
     .catch((error) => console.log(error))
 }
