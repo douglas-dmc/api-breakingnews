b93e9476b287918215a83aa1c5c399fe826bacbe
Douglas Marques <douglasmarques37@gmail.com>
2023-07-29 18:01:01 -0300

    paginação

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,8 @@
-import { createService, findAllService } from "../services/news.service.js"
+import {
+    countNews,
+    createService,
+    findAllService,
+} from "../services/news.service.js"
 
 const create = async (req, res) => {
     try {
@@ -24,12 +28,53 @@ const create = async (req, res) => {
 }
 
 const findAll = async (req, res) => {
-    const news = await findAllService()
+    let { limit, offset } = req.query
+
+    limit = Number(limit)
+    offset = Number(offset)
+
+    if (!limit) {
+        limit = 5
+    }
+    if (!offset) {
+        offset = 0
+    }
+
+    const news = await findAllService(offset, limit)
+    const total = await countNews()
+    const currentUrl = req.base
+
+    const next = offset - limit
+    const nextUrl =
+        next < total ? `${currentUrl}?limit=${limit}&offset=${offset}` : null
+    const previous = offset - limit < 0 ? null : offset - limit
+    const previousUrl =
+        previous != null
+            ? `${currentUrl}?limit=${limit}&offset=${previous}`
+            : null
 
     if (news.length === 0) {
         return res.status(400).send({ message: "There are no registered news" })
     }
-    res.send(news)
+    res.send({
+        nextUrl,
+        previousUrl,
+        limit,
+        offset,
+        total,
+
+        results: news.map(Item=>({
+            id: Item._id,
+            title: Item.title,
+            text: Item.text,
+            banner: Item.banner,
+            likes: Item.likes,
+            comments: Item.comments,
+            name: Item.user.name,
+            userName: Item.user.username,
+            userAvatar: Item.user.avatar
+        }))
+    })
 }
 
 export { create, findAll }
