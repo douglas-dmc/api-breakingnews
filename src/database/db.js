53356102c337014d70754eb8b82ea314e0a890f7
Douglas Marques <douglasmarques37@gmail.com>
2023-08-01 21:55:47 -0300

    alterando config database 2

--- src/database/db.js
+++ src/database/db.js
@@ -3,16 +3,20 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-const hostname = process.env.DB_HOST
+const username = process.env.DB_USER
+const password = process.env.DB_PWD
 
 const connectDatabase = () => {
     console.log("Wait connecting to the database...")
 
     mongoose
-        .connect(hostname, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+        .connect(
+            `mongodb+srv://${username}:${password}@cluster0.vcnesjk.mongodb.net/?retryWrites=true&w=majority`,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            }
+        )
         .then(() => console.log("MongoDB Atlas connected"))
         .catch((error) => console.log(error))
 }
